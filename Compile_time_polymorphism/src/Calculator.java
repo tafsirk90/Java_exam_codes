1ae6a872ef9826a921413a01b3b545ff1049fdc5
tafsirk90 <khantafsir42@gmail.com>
2026-09-03 09:19:27 +0600

    Compile-time and runtime polymrphism

---
+++ Compile_time_polymorphism/src/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+    int add(int a, int b){
+        return a + b;
+    }
+    double add(double a, double b){
+        return a + b;
+    }
+}
---
+++ Compile_time_polymorphism/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println(c.add(1,2));
+        System.out.println(c.add(2.2, 3.2));
+    }
+}
