99e3324e74d8ed6121b14046d23e6afc89aa13a8
tafsirk90 <khantafsir42@gmail.com>
2026-09-03 07:02:10 +0600

    Initial commit

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Tafsir");
+        s.setID(59);
+        System.out.println(s.getName());
+        System.out.println(s.getID());
+    }
+}
---
+++ Encapsulation/src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    private String name;
+    private int id;
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setID(int id) {this.id = id;}
+    public String getName(){return name;}
+    public int getID(){return id;}
+}
